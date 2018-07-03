0f52ccfe5c6edca7afd146e029b6048b1defa953
Sean DMR <sean@flyinggrizzly.io>
2018-07-03 09:46:10 +0100

    Naive implementation of App

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,7 +1,9 @@
 /*****
  * Create a new component that produces some HTML
  */
-
+const App = function() {
+  return <div>Hi!</div>;
+}
 
 /*****
  * Take this component's generated HTML and put it in the DOM
