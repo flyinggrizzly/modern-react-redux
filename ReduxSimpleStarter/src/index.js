1d448ff807a952317a0edce2de5a5945173c9010
Sean DMR <sean@flyinggrizzly.io>
2018-07-03 10:00:18 +0100

    Comments on JSX transpilation

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -2,6 +2,19 @@
  * Create a new component that produces some HTML
  */
 const App = function() {
+  /**
+   * Returns JSX which will be transpiled into JavaScript
+   * calls like
+   *   React.createElement('div', null, 'Hi!');
+   *
+   * If you include attributes on the element...
+   *   return <div class='foo' name='bar' data-whatever='cher'>Hi!</div>;
+   *
+   * the transpiled JS looks like
+   *   React.createElement('div',
+   *                       { 'class' : 'foo', 'name' : 'bar', 'data-whatever' : 'cher' },
+   *                       'Hi!');
+   */
   return <div>Hi!</div>;
 }
 
