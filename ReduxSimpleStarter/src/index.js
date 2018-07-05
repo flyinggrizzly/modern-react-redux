10c1e92812321c5b75d33bd0ec2f311f9161efe5
Sean DMR <sean@flyinggrizzly.io>
2018-07-05 09:47:13 +0100

    Import local search bar component

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -3,6 +3,8 @@ require('dotenv').load();
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = process.env.API_KEY;
 
 /*****
