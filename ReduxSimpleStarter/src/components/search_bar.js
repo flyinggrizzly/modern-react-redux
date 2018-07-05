e64ee560ae057a4964ded8d8753d3e30da2fd48b
Sean DMR <sean@flyinggrizzly.io>
2018-07-05 09:46:55 +0100

    Basic search bar component

---
+++ ReduxSimpleStarter/src/components/search_bar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/**
+ * Functional Component
+ *
+ * - it is a function:
+ *   input goes in,
+ *   JSX comes out
+ */
+const SearchBar = () => {
+  return <input />;
+};
+
+export default SearchBar;
