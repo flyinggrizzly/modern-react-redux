72b6b6daa0c7b425d99830e5aa46e348258fe31e
Sean DMR <sean@flyinggrizzly.io>
2018-07-16 09:38:42 +0100

    Pass initial video list results as properties to VideoList component

--- ReduxSimpleStarter/src/components/video_list.js
+++ ReduxSimpleStarter/src/components/video_list.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
-const VideoList = () => {
+const VideoList = (props)  => {
+  const videos = props.videos;
+  console.table(videos);
   return (
     <ul className='col-md-4 list-group'>
     </ul>
