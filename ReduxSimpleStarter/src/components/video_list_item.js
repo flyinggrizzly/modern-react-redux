6c0fc97a80461424a55ed6f8825e0b15b7d0ee43
Sean DMR <sean@flyinggrizzly.io>
2018-07-19 09:37:38 +0100

    Render video thumbnails and titles in VLItem

--- ReduxSimpleStarter/src/components/video_list_item.js
+++ ReduxSimpleStarter/src/components/video_list_item.js
@@ -1,19 +1,23 @@
 import React from 'react';
 
-/**
- * Using the ({properties_attribute_name}) syntax
- * is ES6 shorthand for passing in a props object
- * and assigning one of its attributes to a variable
- * with the same name within the function.
- *
- * In this case, ({video}) exposes a variable called
- * `video` in the scope of the function.
- *
- * It is equivalent to passing in (props) and then
- * assigning `const video = props.video`.
- */
 const VideoListItem = ({video}) => {
-  return <li>Video</li>;
+  const imageUrl = video.snippet.thumbnails.default.url;
+  const videoTitle = video.snippet.title;
+
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object"
+               src={imageUrl} />
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading">{videoTitle}</div>
+        </div>
+      </div>
+    </li>
+  );
 }
 
 export default VideoListItem;
