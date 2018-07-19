17fff7e78a059fd946f5f199a09d4ab522dca7fb
Sean DMR <sean@flyinggrizzly.io>
2018-07-19 09:29:07 +0100

    Replace long-hand props-to-var assignment with ES6 shorthand

--- ReduxSimpleStarter/src/components/video_list_item.js
+++ ReduxSimpleStarter/src/components/video_list_item.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-const VideoListItem = props => {
-  const video = props.video;
-
-  return <li>Video</li>
+/**
+ * Using the ({properties_attribute_name}) syntax
+ * is ES6 shorthand for passing in a props object
+ * and assigning one of its attributes to a variable
+ * with the same name within the function.
+ *
+ * In this case, ({video}) exposes a variable called
+ * `video` in the scope of the function.
+ *
+ * It is equivalent to passing in (props) and then
+ * assigning `const video = props.video`.
+ */
+const VideoListItem = ({video}) => {
+  return <li>Video</li>;
 }
 
 export default VideoListItem;
