dd5b693a1260a408f87bab3213b69ce10a6d5b90
Sean DMR <sean@flyinggrizzly.io>
2018-08-22 09:51:44 +0100

    Simplify VideoDetail component assignments

--- ReduxSimpleStarter/src/components/video_detail.js
+++ ReduxSimpleStarter/src/components/video_detail.js
@@ -3,11 +3,9 @@ import React from 'react';
 const VideoDetail = ({video}) => {
   if (!video) { return <div>Loading...</div>; }
 
+  const embedUrl = `https://www.youtube.com/embed/${video.id.videoId}`;
   const videoTitle = video.snippet.title;
   const videoDescription = video.snippet.description;
-
-  const videoId = video.id.videoId;
-  const embedUrl = `https://www.youtube.com/embed/${videoId}`;
 
   return(
     <div className="video-detail col-md-8">
