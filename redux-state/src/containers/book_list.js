a7121afdb4613b0db424fdc399f109a770d74e2d
Sean DMR <sean@flyinggrizzly.io>
2018-08-30 13:59:13 +0100

    Connect React component to Redux state

--- redux-state/src/containers/book_list.js
+++ redux-state/src/containers/book_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
   render() {
     return (
       <ul className="list-group col-smr-4">
@@ -19,3 +20,17 @@ export default class BookList extends Component {
     });
   }
 }
+
+/*
+ * Tell ReactRedux::connect how to map state to props in the component.
+ */
+function mapStateToProps(state) {
+  // Whatever is returned here will be available as props in
+  // BookList
+  return {
+    books: state.books
+  }
+}
+
+// Export a container defined by connecting a state map to a component
+export default connect(mapStateToProps)(BookList);
