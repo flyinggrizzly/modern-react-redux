3947e7ef92fb0d9db13bad679b3cbaa8aecddf86
Sean DMR <sean@flyinggrizzly.io>
2018-09-03 17:20:17 +0100

    Create and wire up ActiveBook reducer

--- redux-state/src/reducers/index.js
+++ redux-state/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import BooksReducer from './books_reducer.js';
+import ActiveBookReducer from './active_book_reducer.js';
 
 const rootReducer = combineReducers({
-  books: BooksReducer
+  books: BooksReducer,
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
