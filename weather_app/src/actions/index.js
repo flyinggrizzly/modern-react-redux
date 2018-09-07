e199ac85cc39d5c5df0b5e3f9326941121e0cb3f
Sean DMR <sean@flyinggrizzly.io>
2018-09-07 10:10:43 +0100

    Actually fetch data from API

--- weather_app/src/actions/index.js
+++ weather_app/src/actions/index.js
@@ -1,14 +1,17 @@
+import axios from 'axios';
+
 const API_KEY = 'f97954cae09a9dee1a38a1f46d9e8343';
 const ROOT_URL = 'http://api.openweathermap.org/data/2.5/forecast?';
 const AUTHENTICATED_URL = `${ROOT_URL}appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
+
 export function fetchWeather(city) {
   const url = `${AUTHENTICATED_URL}&q=${city},uk`;
-
+  const request = axios.get(url);
 
   return {
     type: FETCH_WEATHER,
-    payload: null
+    payload: request
   }
 }
