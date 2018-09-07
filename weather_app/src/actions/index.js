f806e038d922ce7bb6e367bdc36f1e96cb2e5fe5
Sean DMR <sean@flyinggrizzly.io>
2018-09-07 10:07:49 +0100

    Begin constructing API query URL

--- weather_app/src/actions/index.js
+++ weather_app/src/actions/index.js
@@ -1,1 +1,14 @@
-const API_KEY = 'b6907d289e10d714a6e88b30761fae22';
+const API_KEY = 'f97954cae09a9dee1a38a1f46d9e8343';
+const ROOT_URL = 'http://api.openweathermap.org/data/2.5/forecast?';
+const AUTHENTICATED_URL = `${ROOT_URL}appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+export function fetchWeather(city) {
+  const url = `${AUTHENTICATED_URL}&q=${city},uk`;
+
+
+  return {
+    type: FETCH_WEATHER,
+    payload: null
+  }
+}
