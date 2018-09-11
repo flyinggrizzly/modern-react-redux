cdcf55dd663e80087af0a9782fe29b257e46d620
Sean DMR <sean@flyinggrizzly.io>
2018-09-11 09:24:17 +0100

    Import Redux state

--- weather_app/src/containers/weather_list.js
+++ weather_app/src/containers/weather_list.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 export default class WeatherList extends Component {
   render() {
@@ -19,3 +20,6 @@ export default class WeatherList extends Component {
   }
 }
 
+function mapStateToProps(state) {
+  return { weather: state.weather };
+}
