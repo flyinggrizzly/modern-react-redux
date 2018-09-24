806db00ab2aac79668952e3cd97cbaea51f00d53
Sean DMR <sean@flyinggrizzly.io>
2018-09-24 09:28:19 +0100

    Bind fetching of posts to component lifecycle

--- simple-blog/src/components/posts/index.js
+++ simple-blog/src/components/posts/index.js
@@ -4,6 +4,13 @@ import { connect } from 'react-redux';
 import { fetchPosts } from '../../actions';
 
 class PostsIndex extends Component {
+  // Lifecycle function--runs once the component has appeared on the DOM
+  // Fetching data could be before or after mounting, because it's async,
+  // and rendering won't get blocked.
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render() {
     return(
       <div>
