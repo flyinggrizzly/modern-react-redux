101680189507ecd3048bf7e08d5c28e1ce407fa5
Sean DMR <sean@flyinggrizzly.io>
2018-10-05 15:30:35 +0100

    Set up show view

--- simple-blog/src/components/posts/show.js
+++ simple-blog/src/components/posts/show.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { getPost } from '../../actions';
 
 class PostsShow extends Component {
+  componentDidMount() {
+    // extract ID from properties provided by react-router-dom
+    this.props.getPost(this.props.match.params.id);
+  }
+
   render() {
+    const { post } = this.props;
+
     return(
       <div>
-        Post show view
+        <h2>{ post.title }</h2>
+        <h6>Categories: { post.categories }</h6>
+        <p>{ post.content }</p>
       </div>
     );
   }
 }
 
-export default PostsShow;
+// ownProps are the props that the component would have intrinsically (aside from the redux state props we will map)
+function mapStateToProps({ posts }, ownProps) {
+  return { post: posts[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { getPost })(PostsShow);
