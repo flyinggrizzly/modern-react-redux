aa9674fc30e3a6c7e3e8ce4283f519f95b4296a1
Sean DMR <sean@flyinggrizzly.io>
2018-10-09 09:30:11 +0100

    Don't wait for index reload to drop deleted post from state

--- simple-blog/src/reducers/posts_reducer.js
+++ simple-blog/src/reducers/posts_reducer.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
 
-import { FETCH_POSTS, GET_POST } from '../actions';
+import { FETCH_POSTS, GET_POST, DELETE_POST } from '../actions';
 
 export default function(state = {}, action) {
   switch (action.type) {
+  case DELETE_POST:
+    return _.omit(state, action.paylod);
   case GET_POST:
     const post = action.payload.data;
     return { ...state, [post.id]: post };
